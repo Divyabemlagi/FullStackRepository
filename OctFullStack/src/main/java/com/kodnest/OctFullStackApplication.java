ce436a69e400eb04c41ef3c5f39846cc7aa1976a
Divyabemlagi <93431309+Divyabemlagi@users.noreply.github.com>
2024-04-22 23:53:42 +0530

    Add files via upload

---
+++ OctFullStack/src/main/java/com/kodnest/OctFullStackApplication.java
@@ -0,0 +1,13 @@
+package com.kodnest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OctFullStackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OctFullStackApplication.class, args);
+	}
+
+}
